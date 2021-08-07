83a44eb853e1feee3ff2a657c9cc098d7c88e3f6
Jacob Valdez <jacobfv123@gmail.com>
2021-08-06 21:21:53 -0500

    experimental

---
+++ .next/static/ckaaPQQASMbOznwwXaHbB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-157366ffea25803f02fd.js"],"/home":["static/chunks/pages/home-c321fe274b6e27bffaa1.js"],sortedPages:["/_app","/_error","/home"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
